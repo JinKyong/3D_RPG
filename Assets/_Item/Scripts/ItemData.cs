c07087ab63b43493e4bf80c6f8a061f5123453a0
JinKyong <48237998+JinKyong@users.noreply.github.com>
2023-06-23 17:15:41 +0900

    itemSO
    SO 생성

---
+++ Assets/_Item/Scripts/ItemData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="ItemData",
+    menuName ="ScriptableObjects/Item/ItemData")]
+public class ItemData : ScriptableObject
+{
+
+}
