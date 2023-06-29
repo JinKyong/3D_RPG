c19ce1818ce6ba77d840c72afad4ca237f8254e4
supportkim0 <135775756+supportkim0@users.noreply.github.com>
2023-06-29 14:46:57 +0900

    ui 애니메이션 보정
    애니메이션 수정하여 좀더 구체적인 애니메이션 을 설정하였습니다.

--- Assets/_UI/Script/UIManager.cs
+++ Assets/_UI/Script/UIManager.cs
@@ -5,10 +5,35 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public GameObject preferencesObject;
+    public Animator[] backAnimators; // 변수 이름을 소문자로 시작하도록 수정
+    public Animator preferences_Btn;
+
+
+    public void Onpreferences_BtnClicked()
+    {
+        preferences_Btn.SetTrigger("RunAnimation");
+    
+    }
+
 
     public void OnPreferencesButtonClicked()
     {
         preferencesObject.SetActive(true);
     }
-}
 
+    public void OnCloseButtonClicked()
+    {
+        foreach (Animator animator in backAnimators)
+        {
+            animator.SetTrigger("Close");
+            StartCoroutine(DisableAfterAnimation(animator)); // Coroutine 내부에서도 animator를 사용할 수 있도록 수정
+        }
+    }
+
+    private IEnumerator DisableAfterAnimation(Animator animator) // IEnumerator에 Animator 파라미터 추가
+    {
+        yield return new WaitForSeconds(2f); // 애니메이션 실행 시간 조절, 필요한 경우 값을 변경하세요
+
+        preferencesObject.SetActive(false);
+    }
+}
