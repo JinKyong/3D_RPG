10ab1478fbb94a0e5fe6ca5292bc5996251dcdfa
supportkim0 <135775756+supportkim0@users.noreply.github.com>
2023-06-30 15:03:46 +0900

    애니메이터 수정

--- Assets/_UI/Script/LoginManager.cs
+++ Assets/_UI/Script/LoginManager.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class LoginManager : MonoBehaviour
+namespace Main.UI
 {
-    public InputField id;        // 유저 아이디 변수.
-
-    public InputField password; // 유저 패스워드 변수.
-
-    public Text notify;         // 중복이 있는지 검사.
-
-    private void Start()
+    public class LoginManager : MonoBehaviour
     {
-        notify.text = "";       // 검사 텍스트 창을 비워준다.
-    }
+        [SerializeField] InputField id;        // 유저 아이디 변수.
 
-    public void SaveUserData()                                  // 아이디와 패스워드 를 저장 하는 함수.
-    {
-        if (!CheckInput(id.text, password.text))                // 만일 입력 검사에 문제가 있을시 함수를 종료.
-            return;
+        [SerializeField] InputField password; // 유저 패스워드 변수.
+
+        [SerializeField] Text notify;         // 중복이 있는지 검사.
+
+        private void Start()
         {
+            notify.text = "";       // 검사 텍스트 창을 비워준다.
+        }
+
+        public void SaveUserData()                                  // 아이디와 패스워드 를 저장 하는 함수.
+        {
+            if (!CheckInput())     // 만일 입력 검사에 문제가 있을시 함수를 종료.
+                return;
+
             if (!PlayerPrefs.HasKey(id.text))                   // 시스템에 저장돼 있는 아이디가 존재하지 않는다면...
             {
                 PlayerPrefs.SetString(id.text, password.text);  // 사용자의 아이디 키 로 패스워드를 값(value)으로 설정하여 저장.
@@ -33,13 +35,12 @@ public class LoginManager : MonoBehaviour
                 notify.text = "이미 존재하는 아이디입니다.!";
             }
 
+
         }
-    }
-    public void CheckUserData()                            // 로그인 을 위해 확인을 필요로한 함수.
-    {
-        if (CheckInput(id.text, password.text))            // 만일 입력 검사에 문제가 있으면 함수를 종료. 
-            return;
+        public void CheckUserData()                            // 로그인 을 위해 확인을 필요로한 함수.
         {
+            if (CheckInput())            // 만일 입력 검사에 문제가 있으면 함수를 종료. 
+                return;
             string pass = PlayerPrefs.GetString(id.text);   //  사용자가 입력한 아이디를 키로 사용해 시스템에 저장된 값을 불러온다.
 
             if (password.text == pass)                      // 만일, 사용자가 입력한 패스워드와 시스템에서 불러오 ㄴ값을 비교해서 동일하다면!
@@ -50,21 +51,23 @@ public class LoginManager : MonoBehaviour
             {
                 notify.text = "일력하신 아이디와 패스워드가 일치하지 않습니다.!";    // 그렇지 않고 두 데이터 값이 다르면 , 유저 정보가 일치하지 않아 메시지를 남김.
             }
-        }
-    }
-    bool CheckInput(string id, string pwd)                      // 입력 완료 확인 함수
-    {
-        if (id == "" || pwd == "")                              // 만일, 입력란이 하나라도 비어 있으면 유저 정보 입력을 요구한다.
-        {
-            notify.text = "아이디 또는 패스워드를 입력해주세요";
-            return false;
-        }
-                                                                    // 입력이 비어 있지 않으면 ture를 반환한다.
-        else
-        {
-            return true;
-        }
 
+        }
+        bool CheckInput()                      // 입력 완료 확인 함수
+        {
+            if (id.text == "" || password.text == "")                              // 만일, 입력란이 하나라도 비어 있으면 유저 정보 입력을 요구한다.
+            {
+                notify.text = "아이디 또는 패스워드를 입력해주세요";
+                return false;
+            }
+            // 입력이 비어 있지 않으면 ture를 반환한다.
+            else
+            {
+                return true;
+            }
+
+        }
     }
-}    
+}
+
     
--- Assets/_UI/Script/SoundManager.cs
+++ Assets/_UI/Script/SoundManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
+namespace Main.Sound
 {
-    public AudioSource bgMusic;         // 배경 음악을 재생하는 AudioSource 컴포넌트를 참조하는 변수
-
-    public void SetMusicVolume(float volume)
+    public class SoundManager : MonoBehaviour
     {
-        bgMusic.volume = volume;         // 전달받은 볼륨 값을 배경 음악의 볼륨에 할당
+        public AudioSource bgMusic;         // 배경 음악을 재생하는 AudioSource 컴포넌트를 참조하는 변수
+
+        public void SetMusicVolume(float volume)
+        {
+            bgMusic.volume = volume;         // 전달받은 볼륨 값을 배경 음악의 볼륨에 할당
+        }
     }
 }
