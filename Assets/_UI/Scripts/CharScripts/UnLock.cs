36ea05350729804e7e4046fc39b99090b37ea323
supportkim0 <135775756+supportkim0@users.noreply.github.com>
2023-07-10 17:39:58 +0900

    ui 이미지 제작 및 ui 배치

---
+++ Assets/_UI/Scripts/CharScripts/UnLock.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnLock                        // 오브젝트풀.
+{ 
+public class UnLock : MonoBehaviour
+{
+ 
+
+
+}
+
+}
