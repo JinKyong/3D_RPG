cb84b3d08011fd1c7f902e75a35073f577918f65
supportkim0 <135775756+supportkim0@users.noreply.github.com>
2023-07-28 14:52:11 +0900

    네임스페이스 수정및 유아이 추가

--- Assets/_UI/Scripts/PlayerName.cs
+++ Assets/_UI/Scripts/PlayerName.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerName: MonoBehaviour
-{
-    private Transform target;
-
-    private void Start()
+namespace PlayerName
+{ 
+    public class PlayerName: MonoBehaviour
     {
-        target = Camera.main.gameObject.transform;
-    }
+        private Transform target;
 
-    private void LateUpdate()
-    {
-        transform.rotation = Quaternion.LookRotation(target.forward, target.up);
+        private void Start()
+        {
+            target = Camera.main.gameObject.transform;
+        }
+
+        private void LateUpdate()
+        {
+            transform.rotation = Quaternion.LookRotation(target.forward, target.up);
+        }
     }
 }
+
--- Assets/_UI/Scripts/PreferencesSetting.cs
+++ Assets/_UI/Scripts/PreferencesSetting.cs
@@ -1,26 +1,31 @@
 using UnityEngine;
-public class PreferencesSetting : MonoBehaviour
-{
-    Animator anim;
-    private void Start()
+
+namespace PreferencesSetting
+{ 
+    public class PreferencesSetting : MonoBehaviour
     {
-        anim = GetComponent<Animator>();
-        // 추가: PreferencesSetting 게임 오브젝트 활성화
-        gameObject.SetActive(true);
+        Animator anim;
+        private void Start()
+        {
+            anim = GetComponent<Animator>();
+            // 추가: PreferencesSetting 게임 오브젝트 활성화
+            gameObject.SetActive(true);
+        }
+        public void PreferencesSettingClick()
+        {
+            bool btn = anim.GetBool("PreferencesBtn");
+            anim.SetBool("PreferencesBtn", !btn);
+        }
+        public void PreferencesSideOnclick()
+        {
+            bool sideBtn = anim.GetBool("Side");
+            anim.SetBool("Side", !sideBtn);
+        }
+        public void PreferencesSideBackOnclick()
+        {
+            bool sideBackBtn = anim.GetBool("SideBack");
+            anim.SetBool("SideBack", !sideBackBtn);
+        }
     }
-    public void PreferencesSettingClick()
-    {
-        bool btn = anim.GetBool("PreferencesBtn");
-        anim.SetBool("PreferencesBtn", !btn);
-    }
-    public void PreferencesSideOnclick()
-    {
-        bool sideBtn = anim.GetBool("Side");
-        anim.SetBool("Side", !sideBtn);
-    }
-    public void PreferencesSideBackOnclick()
-    {
-        bool sideBackBtn = anim.GetBool("SideBack");
-        anim.SetBool("SideBack", !sideBackBtn);
-    }
+
 }
