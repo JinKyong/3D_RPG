d599646571f2194d66e2f3db55fd9e7c1dd01965
inhyeok <oredtiger@naver.com>
2023-08-04 14:43:00 +0900

    playerstat

---
+++ Assets/_Character/Player/Data/PlayerStat.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    [CreateAssetMenu(fileName ="PlayerStat",
+        menuName ="ScriptableObjects/Player/Stat")]
+    public class PlayerStat : ScriptableObject, ISerializationCallbackReceiver
+    {
+        //Initial Value
+        [SerializeField] float initHealth;
+
+        //Runtime Value
+        public float runTimeHealth;
+
+        public void OnBeforeSerialize()
+        {
+
+        }
+
+        public void OnAfterDeserialize()
+        {
+            //runTimeHealth = initHealth;
+        }
+    }
+}
